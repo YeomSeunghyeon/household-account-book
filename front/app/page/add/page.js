2ef7aca54e7d5e6f98677e3ca104ac4cf9ba4a61
YeomSeunghyeon <100lion67@gmail.com>
2025-02-12 02:21:37 +0900

    2-12

--- front/app/page/add/page.js
+++ front/app/page/add/page.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import "./add.css";
 import axios from "axios";
-
+import Select from "react-select";
 
 export default function add(){
     const [title,setTitle]=useState("");
@@ -10,6 +10,8 @@ export default function add(){
     const [num,setNum]=useState("");
     const [detail,setDetail]=useState("");
     const [date,setDate]=useState("");
+    const [category,setCategory]=useState([]);
+    const [selectedOption, setSelectedOption] = useState(null); 
     useEffect(() => {
         const interval = setInterval(() => {
           setDate(new Date().toLocaleString()); // 현재 시간 갱신
@@ -19,26 +21,45 @@ export default function add(){
       }, []);
 
       useEffect(()=>{
-      
+       categoryData();
       },[])
+      const categoryData=async()=>{
+        try{
+           const response=await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/category/all`);
+           setCategory(response.data);
+        }catch(error){
+          if (error.response.status === 500) {
+              alert(error.response.data.message);
+          } else if (error.response.status === 401 || error.response.status === 403) {
+              alert(error.response.data.message);
+          }
+        }
+       };
     return(
         <div className="addAll">
             <div className="addText">지출 추가하기</div>
-            <div>
-                <div>지출 이름</div>
-                <input className="titleInput" onChange={(e)=>setTitle(e.target.value)}/>
+            <div className="titleAll">
+                <div className="titleText">지출 이름</div>
+                <textarea className="titleInput" onChange={(e)=>setTitle(e.target.value)}/>
             </div>
-            <div>
-                <div>지출액</div>
+            <div className="priceAll">
+                <div  className="priceText">지출액(원 생략)</div>
                 <input className="priceInput" onChange={(e)=>setPrice(e.target.value)}/>
             </div>
-            <div>
-                <div>카테고리</div>
-                
+            <div className="categoryAll">
+                <div  className="categoryText">카테고리</div>
+                <Select
+                  options={category}
+                  getOptionLabel={(e)=>e.name}
+                  getOptionValue={(e)=>e.num}
+                  value={num}
+                  onChange={setNum}
+                  placeholder="카테고리 선택"
+                  className="categorySelect"/>
             </div>
-            <div>
-                <div>지출 내용</div>
-                <input className="detailInput" onChange={(e)=>setDetail(e.target.value)}/>
+            <div className="detailAll">
+                <div  className="detailText">지출 내용</div>
+                <textarea className="detailInput" onChange={(e)=>setDetail(e.target.value)}/>
             </div>
         </div>
     )
